f89019bdbfc87d3f5c4ed56f6a35082ed83b4ddf
Simon Elvery <simon@elvery.net>
2017-09-05 10:54:54 +1000

    New bubble styles

--- src/components/message-bubble/index.js
+++ src/components/message-bubble/index.js
@@ -3,16 +3,18 @@ import Preact from 'preact';
 import style from './style.scss';
 
 export default class MessageBubble extends Preact.Component {
-  render() {
+  render({ text, hide, side, status }) {
     return (
       <div className={style.bubbleContainer}>
-        <div className={`${style.bubbleInner} ${style[this.props.side]} ${this.props.hide ? style.hide : ''}`}>
+        <div className={`${style.bubbleInner} ${style[side]} ${hide ? style.hide : ''}`}>
           <div className={style.bubble}>
-            {this.props.text}
+            {text}
+            {status
+              ? <div className={style.status}>
+                  {status}
+                </div>
+              : null}
           </div>
-        </div>
-        <div className={style.status}>
-          {this.props.status}
         </div>
       </div>
     );
